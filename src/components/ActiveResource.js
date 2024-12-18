9c427b7ad1c16867699129a8a738a8d402d46d68
John Doe <johndoe@example.com>
2024-12-18 12:19:18 +0100

    Display seconds - useEffect

--- src/components/ActiveResource.js
+++ src/components/ActiveResource.js
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 
 const ActiveResource = () => {
   const [resource, setResource] = useState({});
+  const [seconds, setSeconds] = useState();
 
   useEffect(() => {
     async function fetchActiveResource() {
@@ -21,6 +22,7 @@ const ActiveResource = () => {
 
       if (updateTimeToFinish >= 0) {
         resource.timeToFinish = updateTimeToFinish;
+        setSeconds(updateTimeToFinish);
       }
       // alert(updateTimeToFinish);
 
@@ -29,11 +31,23 @@ const ActiveResource = () => {
     fetchActiveResource();
   }, []);
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSeconds(seconds - 1);
+    }, 1000);
+    if (seconds < 0) {
+      clearInterval(interval);
+    }
+    // clean component
+    return () => clearInterval(interval);
+  }, [seconds]);
+
   return (
     <div className="active-resource">
       <h1 className="resource-name">{resource.title}</h1>
       <div className="time-wrapper">
-        <h2 className="elapsed-time">{resource.timeToFinish}</h2>
+        {/* <h2 className="elapsed-time">{resource.timeToFinish}</h2> */}
+        <h2 className="elapsed-time">{seconds}</h2>
       </div>
       <Link href="/" className="button">
         Go to resource
